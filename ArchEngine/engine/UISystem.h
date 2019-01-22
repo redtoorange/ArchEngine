ab7fb0b2d8f29299b5852f8a356d160354ade871
Andrew McGuiness <redtoorange@gmail.com>
2019-01-22 10:48:06 -0500

    Integrate nuklear for UI system

---
+++ ArchEngine/engine/UISystem.h
@@ -0,0 +1,27 @@
+﻿// #pragma once
+// #include <SDL.h>
+//
+// struct nk_context;
+// struct nk_colorf;
+//
+// namespace arch {
+// 	class UISystem {
+// 	public:
+// 		UISystem();
+//
+// 		virtual ~UISystem();
+//
+// 		void Update();
+//
+// 		void Input(SDL_Event& event);
+//
+// 		void Draw();
+//
+// 		void Init();
+//
+// 	private:
+// 		nk_context* ctx;
+// 		nk_colorf* bg;
+// 	};
+//
+// }
